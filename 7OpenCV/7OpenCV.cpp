17502665d9d82d858c8787ca81fd04c021462bc2
Ігор Коваль <koval_ak19@nuwm.edu.ua>
2022-12-24 21:54:17 +0200

    lab7

---
+++ 7OpenCV/7OpenCV.cpp
@@ -0,0 +1,20 @@
+﻿#include <opencv.hpp>
+
+#include <core.hpp>
+
+#include <highgui.hpp>
+
+int main()
+{
+    cv::Mat image = cv::imread("1.png");
+
+    cv::namedWindow("Image:", 1);
+
+    cv::imshow("Image:", image);
+
+    cv::waitKey();
+
+    cv::destroyWindow("Image:");
+
+    return 0;
+}
